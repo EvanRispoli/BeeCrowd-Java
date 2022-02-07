e5fa72976de22a771b3f36e941c52216c6e934a4
Evan Rispoli <evan.rispoli@gmail.com>
2022-02-07 02:49:14 -0300

    URI 1000

---
+++ src/main/java/Beginner/HelloWorld.java
@@ -0,0 +1,11 @@
+package Beginner;
+
+
+//Your first program in any programming language is usually "Hello World!".
+// In this first problem all you have to do is print this message on the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
